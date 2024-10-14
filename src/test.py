920cb2f6c18577c445d9e40021d1a57d9b55360e
Anand A. Joshi <ajoshi@usc.edu>
2024-10-14 12:19:01 -0700

    update

--- src/test.py
+++ src/test.py
@@ -46,18 +46,6 @@ from tqdm import tqdm
 
 import torch
 
-
-date_time = time.strftime("%m_%d_%Y_%H_%M")
-
-##
-
-model_name = "best_metric_model_10_14_2024_augmentation" + date_time
-
-##
-
-
-print("Name of the model: ", model_name)
-
 print_config()
 
 # %%
@@ -146,8 +134,8 @@ val_transform = Compose(
 )
 
 # %%
-headreco_dir = '/project/ajoshi_27/headreco_out/'
-root_dir = '/project/ajoshi_1183/Projects/CRSeg/models'
+headreco_dir = '/home/ajoshi/project_ajoshi_27/headreco_out/'
+root_dir = '/home/ajoshi/Projects/CRSeg/models'
 mode = 'train'
 
 # Read the list of subjects
@@ -172,15 +160,15 @@ for subname in tqdm(sub_lst):
 data_dicts = [{"t1_image": t1_name, "t2_image": t2_name , "label": label_name} for t1_name, t2_name, label_name in zip(train_t1_images, train_t2_images, train_labels)]
 train_files, val_files = data_dicts[:-9], data_dicts[-9:]
 
-train_ds = CacheDataset(data=train_files, transform=train_transform, cache_rate=1.0, num_workers=4)
-#train_ds = Dataset(data=train_files, transform=train_transform)
+#train_ds = CacheDataset(data=train_files, transform=train_transform, cache_rate=1.0, num_workers=4)
+train_ds = Dataset(data=train_files, transform=train_transform)
 
 # use batch_size=2 to load images and use RandCropByPosNegLabeld
 # to generate 2 x 4 images for network training
 train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)
 
-val_ds = CacheDataset(data=val_files, transform=val_transform, cache_rate=1.0, num_workers=4)
-#val_ds = Dataset(data=val_files, transform=val_transform)
+#val_ds = CacheDataset(data=val_files, transform=val_transform, cache_rate=1.0, num_workers=4)
+val_ds = Dataset(data=val_files, transform=val_transform)
 val_loader = DataLoader(val_ds, batch_size=2, num_workers=4)
 
 
@@ -188,31 +176,27 @@ val_loader = DataLoader(val_ds, batch_size=2, num_workers=4)
 # ## Check data shape and visualize
 
 # %%
-for jj in range(5):
-    val_data_example = train_ds[jj]
-    print(f"image shape: {val_data_example['t1_image'].shape}")
-    print(f"image shape: {val_data_example['t2_image'].shape}")
+for jj in range(1):
+    train_data_example = train_ds[jj]
+    print(f"image shape: {train_data_example['t1_image'].shape}")
+    print(f"image shape: {train_data_example['t2_image'].shape}")
 
     plt.figure("t1 and t2 images", (12, 3))
-    plt.subplot(1, 3, 1)
+    plt.subplot(2, 3, 1)
     plt.title(f"t1 image ")
-    plt.imshow(val_data_example["t1_image"][0, :, :, 48].detach().cpu(), cmap="gray")
-    plt.subplot(1, 3, 2)
+    plt.imshow(train_data_example["t1_image"][0, :, :, 48].detach().cpu(), cmap="gray")
+    plt.subplot(2, 3, 2)
     plt.title(f"t2 image")
-    plt.imshow(val_data_example["t2_image"][0, :, :, 48].detach().cpu(), cmap="gray")
-    plt.subplot(1, 3, 3)
+    plt.imshow(train_data_example["t2_image"][0, :, :, 48].detach().cpu(), cmap="gray")
+    plt.subplot(2, 3, 3)
     plt.title(f"mixed image ")
-    plt.imshow(val_data_example["image"][0, :, :, 48].detach().cpu(), cmap="gray")
-    plt.show()
-
-
+    plt.imshow(train_data_example["image"][0, :, :, 48].detach().cpu(), cmap="gray")
+    
     # also visualize the 3 channels label corresponding to this image
-    print(f"label shape: {val_data_example['label'].shape}")
-    plt.figure("label", (12, 3))
     for i in range(3):
-        plt.subplot(1, 3, i + 1)
+        plt.subplot(2, 3, i + 4)
         plt.title(f"label channel {i}")
-        plt.imshow(val_data_example["label"][i, :, :, 48].detach().cpu())
+        plt.imshow(train_data_example["label"][i, :, :, 48].detach().cpu())
     plt.show()
 
 
@@ -271,143 +255,6 @@ torch.backends.cudnn.benchmark = True
 # ## Execute a typical PyTorch training process
 
 # %%
-best_metric = -1
-best_metric_epoch = -1
-best_metrics_epochs_and_time = [[], [], []]
-epoch_loss_values = []
-metric_values = []
-metric_values_tc = []
-metric_values_wt = []
-metric_values_et = []
-
-total_start = time.time()
-for epoch in range(max_epochs):
-    epoch_start = time.time()
-    print("-" * 10)
-    print(f"epoch {epoch + 1}/{max_epochs}")
-    model.train()
-    epoch_loss = 0
-    step = 0
-    for batch_data in train_loader:
-        step_start = time.time()
-        step += 1
-        inputs, labels = (
-            batch_data["image"].to(device),
-            batch_data["label"].to(device),
-        )
-
-        optimizer.zero_grad()
-
-
-        with torch.cuda.amp.autocast():
-            outputs = model(inputs)
-            loss = loss_function(outputs, labels)
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
-        epoch_loss += loss.item()
-        print(
-            f"{step}/{len(train_ds) // train_loader.batch_size}"
-            f", train_loss: {loss.item():.4f}"
-            f", step time: {(time.time() - step_start):.4f}"
-        )
-    lr_scheduler.step()
-    epoch_loss /= step
-    epoch_loss_values.append(epoch_loss)
-    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
-
-    if (epoch + 1) % val_interval == 0:
-        model.eval()
-        with torch.no_grad():
-            for val_data in val_loader:
-                val_inputs, val_labels = (
-                    val_data["t1_image"].to(device),
-                    val_data["label"].to(device),
-                )
-                val_outputs = inference(val_inputs)
-                val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
-                dice_metric(y_pred=val_outputs, y=val_labels)
-                dice_metric_batch(y_pred=val_outputs, y=val_labels)
-
-            metric = dice_metric.aggregate().item()
-            metric_values.append(metric)
-            metric_batch = dice_metric_batch.aggregate()
-            metric_tc = metric_batch[0].item()
-            metric_values_tc.append(metric_tc)
-            metric_wt = metric_batch[1].item()
-            metric_values_wt.append(metric_wt)
-            metric_et = metric_batch[2].item()
-            metric_values_et.append(metric_et)
-            dice_metric.reset()
-            dice_metric_batch.reset()
-
-            if metric > best_metric:
-                best_metric = metric
-                best_metric_epoch = epoch + 1
-                best_metrics_epochs_and_time[0].append(best_metric)
-                best_metrics_epochs_and_time[1].append(best_metric_epoch)
-                best_metrics_epochs_and_time[2].append(time.time() - total_start)
-                torch.save(
-                    model.state_dict(),
-                    os.path.join(root_dir, model_name + "best_metric_model.pth"),
-                )
-                print("saved new best metric model")
-            print(
-                f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
-                f" tc: {metric_tc:.4f} wt: {metric_wt:.4f} et: {metric_et:.4f}"
-                f"\nbest mean dice: {best_metric:.4f}"
-                f" at epoch: {best_metric_epoch}"
-            )
-    print(f"time consuming of epoch {epoch + 1} is: {(time.time() - epoch_start):.4f}")
-total_time = time.time() - total_start
-print(f"train completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}")
-
-# %%
-
-
-# %%
-print(f"train completed, best_metric: {best_metric:.4f} at epoch: {best_metric_epoch}, total time: {total_time}.")
-
-# %% [markdown]
-# ## Plot the loss and metric
-
-# %%
-plt.figure("train", (12, 6))
-plt.subplot(1, 2, 1)
-plt.title("Epoch Average Loss")
-x = [i + 1 for i in range(len(epoch_loss_values))]
-y = epoch_loss_values
-plt.xlabel("epoch")
-plt.plot(x, y, color="red")
-plt.subplot(1, 2, 2)
-plt.title("Val Mean Dice")
-x = [val_interval * (i + 1) for i in range(len(metric_values))]
-y = metric_values
-plt.xlabel("epoch")
-plt.plot(x, y, color="green")
-plt.show()
-
-plt.figure("train", (18, 6))
-plt.subplot(1, 3, 1)
-plt.title("Val Mean Dice TC")
-x = [val_interval * (i + 1) for i in range(len(metric_values_tc))]
-y = metric_values_tc
-plt.xlabel("epoch")
-plt.plot(x, y, color="blue")
-plt.subplot(1, 3, 2)
-plt.title("Val Mean Dice WT")
-x = [val_interval * (i + 1) for i in range(len(metric_values_wt))]
-y = metric_values_wt
-plt.xlabel("epoch")
-plt.plot(x, y, color="brown")
-plt.subplot(1, 3, 3)
-plt.title("Val Mean Dice ET")
-x = [val_interval * (i + 1) for i in range(len(metric_values_et))]
-y = metric_values_et
-plt.xlabel("epoch")
-plt.plot(x, y, color="purple")
-plt.show()
-
 # %% [markdown]
 # ## Check best pytorch model output with the input image and label
 
@@ -416,7 +263,7 @@ model.load_state_dict(torch.load(os.path.join(root_dir, "best/best_metric_model.
 model.eval()
 with torch.no_grad():
     # select one image to evaluate and visualize the model output
-    val_input = val_ds[6]["image"].unsqueeze(0).to(device)
+    val_input = val_ds[6]["t2_image"].unsqueeze(0).to(device)
     roi_size = (96, 96, 96)
     sw_batch_size = 4
     val_output = inference(val_input)
@@ -424,26 +271,22 @@ with torch.no_grad():
 
 
     val_output = post_trans(val_output[0])
-    plt.figure("image", (24, 6))
-    for i in range(1):
-        plt.subplot(1, 4, i + 1)
-        plt.title(f"image channel {i}")
-        plt.imshow(val_ds[6]["image"][i, :, :, 48].detach().cpu(), cmap="gray")
-    plt.show()
-    
+    plt.figure("t2_image", (24, 6))
+    plt.subplot(3, 3, 1)
+    plt.title(f"image channel {i}")
+    plt.imshow(val_ds[6]["t2_image"][0, :, :, 48].detach().cpu(), cmap="gray")
+        
     # visualize the 3 channels label corresponding to this image
-    plt.figure("label", (18, 6))
     for i in range(3):
-        plt.subplot(1, 3, i + 1)
+        plt.subplot(3, 3, i + 4)
         plt.title(f"label channel {i}")
         plt.imshow(val_ds[6]["label"][i, :, :, 48].detach().cpu())
-    plt.show()
     # visualize the 3 channels model output corresponding to this image
-    plt.figure("output", (18, 6))
     for i in range(3):
-        plt.subplot(1, 3, i + 1)
+        plt.subplot(3, 3, i + 7)
         plt.title(f"output channel {i}")
         plt.imshow(val_output[i, :, :, 48].detach().cpu())
+    
     plt.show()
 
 # %% [markdown]
@@ -501,7 +344,7 @@ post_transforms = Compose(
 )
 
 # %%
-model.load_state_dict(torch.load(os.path.join(root_dir, "best_metric_model.pth")))
+model.load_state_dict(torch.load(os.path.join(root_dir, "best/best_metric_model.pth")))
 model.eval()
 device = 'cuda:0'
 with torch.no_grad():
